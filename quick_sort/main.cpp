ec6653faba4e9af94faa0b4861c037aa386ccea9
cmg189 <cmg189@outlook.com>
2022-09-12 17:00:58 -0500

    Added preloaded data for testing

--- quick_sort/main.cpp
+++ quick_sort/main.cpp
@@ -3,8 +3,18 @@
 int main(){
 	cout << "\nQuick Sort Algorithm\n";
 
-	vector<int> data = user_data();
-	
+	// comment out for testing
+	//vector<int> data = user_data();
+	//int starting_index = 0;
+	//int ending_index = data.size() -1;
+
+	// testing purposes
+	vector<int> test_data{12, 99, 100, 32, 57, 18, 1, 56, 83, 44, 7};
+	int starting_index = 0;
+	int ending_index = test_data.size() -1;
+
+
+
 
 	cout << "\nProgram ended\n";
 	return 0;

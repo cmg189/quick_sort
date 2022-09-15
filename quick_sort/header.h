1275ebeab8abe1fa07223a8510d45168194b4b44
cmg189 <cmg189@outlook.com>
2022-09-14 21:27:43 -0500

    Major rework of partition() and quick_sort()

--- quick_sort/header.h
+++ quick_sort/header.h
@@ -2,6 +2,40 @@
 #include <vector>
 using namespace std;
 
+class Flag {
+	public:
+		Flag(int index, bool flag);	// constructor
+
+		void set_index(int index);	// access and mutate index
+		int get_index();
+
+		void set_flag(bool flag);		// access and mutate flag
+		bool get_flag();
+
+	private:
+		int partition_index;
+		bool partition_flag;				// used when
+};
+
+Flag:: Flag(int index, bool flag){
+	this-> partition_index = index;
+	this-> partition_flag = flag;
+	return;
+}
+
+void Flag:: set_index(int index){
+	this->partition_index = index;
+	return;
+}
+int Flag:: get_index(){ return partition_index; }
+
+void Flag:: set_flag(bool flag){
+	this->partition_flag = flag;
+	return;
+}
+bool Flag:: get_flag(){ return partition_flag; }
+
+
 // returns a vector initalized by user
 vector<int> user_data();
 
@@ -10,7 +44,7 @@ vector<int> user_data();
 vector<int> quick_sort(vector<int> data, int bottom, int top);
 
 // returns partition index to continue sorting
-int partition(vector<int> data, int bottom, int top);
+Flag partition(vector<int> &data, int bottom, int top);
 
 // swaps two elements
 vector<int> swap(vector<int> data, int bottom_compare, int top_compare);
@@ -18,7 +52,6 @@ vector<int> swap(vector<int> data, int bottom_compare, int top_compare);
 vector<int> user_data(){
 
 	cout << "\nEnter integers to be sorted\n" << "When finished enter -999\n\n";
-
 
 	int user_num;
 	vector<int> data;						// holds user inputs
@@ -34,7 +67,7 @@ vector<int> user_data(){
 
 	}// while
 
-	cout << "\nYou entered: ";
+	cout << "\nYou entered:\n";
 	for(int i=0; i< data.size(); i++){
 		cout << data.at(i) << " ";
 	}
@@ -45,46 +78,64 @@ vector<int> user_data(){
 
 
 vector<int> quick_sort(vector<int> data, int bottom, int top){
-	//cout << "b: " << bottom << " t: " << top << "\n";
+
 
 	if(bottom < top){
-		int partition_index = partition(data, bottom, top);
+		Flag control = partition(data, bottom, top);
 
-		data = quick_sort(data, bottom, partition_index);		// sorting bottom half of vector
-		data = quick_sort(data, partition_index +1, top);		// sorting top half of the vector
+		data = quick_sort(data, bottom, control.get_index());		// sorting bottom half of vector
+		//cout << "pi " << control.get_index()+1 << " top " << top << endl;
+		data = quick_sort(data, control.get_index() +1, top);		// sorting top half of the vector
+
+
 	}
 
-	for(int i=0; i< data.size(); i++){ cout << data.at(i) << " "; } cout << endl;
+	//for(int i=0; i< data.size(); i++){ cout << data.at(i) << " "; } cout << endl;
 	return data;
 }
 
 
-int partition(vector<int> data, int bottom, int top){
+Flag partition(vector<int> &data, int bottom, int top){
 
 	// itterating elements
 	int pivot = data[bottom];
 	int top_compare = top;
-	int bottom_compare = bottom;
+	int bottom_compare = bottom; // bottom is the pivot index
+	//cout << "pivot " << data[bottom]  <<" bc " << bottom_compare << " tc " << top_compare << endl;
 
+
+	// loop and swap until top_compare itterates past bottom_compare
 	while(bottom_compare < top_compare){
 
-		do{ bottom_compare++; }while(data[bottom_compare] <= pivot);
-		//if(data[bottom_compare] <= pivot){ bottom_compare++; }
+		do{ bottom_compare++; }while(data[bottom_compare] <= pivot );		// stop at element that is >= pivot (can itterate then compare)
+		//do{ top_compare--; }while(data[top_compare] > pivot);
+		for(int i=0; data[top_compare] > pivot; i++){ top_compare--; }	// stop at element that is < pivot (must compare then itterate)
+		//cout << "stop bc " << bottom_compare << " tc " << top_compare << endl;
 
-		do{ top_compare--; }while(data[top_compare] > pivot);
-		//if(data[top_compare] > pivot){ top_compare--; }
-
+		// pivot not yet found, swapping bc and tc
 		if(bottom_compare < top_compare){
-			vector<int> semi_sorted = swap(data, bottom_compare, top_compare);
+			//cout << "swap bc tc " << data[bottom_compare] << " <-> " << data[top_compare] << endl;
+			data = swap(data, bottom_compare, top_compare);
 		}
 	}// while
 
-	swap(data, bottom, top_compare);
+	// occurs when no element is >= pivot (already sorted) set bc back to pivot index (bottom)
+	if(bottom_compare >= data.size()){ bottom_compare = bottom; }
 
-	return top_compare;
+	//cout << "swap pv tc " << data[bottom] << " <-> " << data[top_compare] << " bc " << bottom_compare << endl;
+	// found new pivot position (when bc passes tc)
+	data = swap(data, bottom, top_compare);
+
+
+	//cout << "thr> "; for(int i=0; i< data.size(); i++){ cout << data.at(i) << " "; } cout << endl;
+	//cout << "tc " << top_compare << endl;
+	// flag is false bc we are not finished sorting bottom/top half
+	Flag control(top_compare, false);
+	return control;
 }
 
 vector<int> swap(vector<int> data, int bottom_compare, int top_compare){
+	//cout << "one> "; for(int i=0; i< data.size(); i++){ cout << data.at(i) << " "; } cout << endl;
 
 	// array to hold elements in vector
 	int temp_data[data.size()];
@@ -104,5 +155,7 @@ vector<int> swap(vector<int> data, int bottom_compare, int top_compare){
 		data.push_back(temp_data[i]);
 	}
 
+	//cout << "two> "; for(int i=0; i< data.size(); i++){ cout << data.at(i) << " "; } cout << endl;
+	// return swapped data
 	return data;
 }
